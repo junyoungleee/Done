92b6d380d94f84d227c7b604d2c8cb36cf66bcc6
jun991203 <jun991203@naver.com>
2022-03-01 17:27:26 +0900

    FEAT : 캘린더 모듈 설치 및 수정 & 불필요 파일 삭제

---
+++ app/src/main/java/com/palette/done/view/main/DoneActivity.kt
@@ -0,0 +1,14 @@
+package com.palette.done.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.palette.done.R
+
+class DoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_done)
+    }
+
+
+}
--- app/src/main/java/com/palette/done/view/main/MainActivity.kt
+++ app/src/main/java/com/palette/done/view/main/MainActivity.kt
@@ -2,7 +2,9 @@ package com.palette.done.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.palette.done.R
 import com.palette.done.databinding.ActivityMainBinding
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setCalendarView()
+    }
+
+    private fun setCalendarView() {
+        with(binding.calendarView) {
+            setHeaderTypeface(resources.getFont(R.font.spoqa_han_sans_neo_bold))
+            setTodayTypeface(resources.getFont(R.font.spoqa_han_sans_neo_bold))
+            setTypeface(resources.getFont(R.font.spoqa_han_sans_neo_regular))
+        }
     }
 }
